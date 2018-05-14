bcafbc0800db25772f21edf5c42faf6edfa85c1d
Michel Naud <michel.naud@sfam.eu>
2018-05-14 09:06:59 +0000

    fix app_dev.php to wroks with docker container

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,18 +3,22 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
-// for more information
-//umask(0000);
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR']) || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) ||
-        substr(@$_SERVER['REMOTE_ADDR'], 0, 7) == "192.168" ||
-        php_sapi_name() === 'cli-server')
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+if (!(in_array(@$ip, ['127.0.0.1', 'fe80::1', '::1'])
+    || php_sapi_name() === 'cli-server'
+    || (ip2long(@$ip) & ip2long('192.168.0.0')) == ip2long('192.168.0.0')
+    || (ip2long(@$ip) & ip2long('172.16.0.0')) == ip2long('172.16.0.0')
+)
 ) {
     header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information. (ip: '.$ip.')');
 }
 
 /**
@@ -23,9 +27,9 @@ if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
-$kernel   = new AppKernel('dev', true);
+$kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
-$request  = Request::createFromGlobals();
+$request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
