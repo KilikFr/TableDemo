c446cad1cda1d6a19c1c555f130a9c5d8b9421a5
Michel Naud <michel.naud@sfam.eu>
2018-11-12 13:12:57 +0000

    quick fix of api demo, with last kilik table bundle version

--- src/Kilik/TableDemoBundle/Services/Api/UserService.php
+++ src/Kilik/TableDemoBundle/Services/Api/UserService.php
@@ -5,6 +5,7 @@ namespace Kilik\TableDemoBundle\Services\Api;
 use GuzzleHttp\ClientInterface;
 use Kilik\TableBundle\Api\ApiInterface;
 use Kilik\TableBundle\Api\StandardResult;
+use Kilik\TableBundle\Components\TableInterface;
 
 class UserService implements ApiInterface
 {
@@ -26,27 +27,24 @@ class UserService implements ApiInterface
     /**
      * {@inheritdoc}
      */
-    public function load($filters, $orderBy = [], $offset = null, $limit = null)
+    public function load(TableInterface $table, $filters, $orderBy = [], $page = null, $limit = null)
     {
         // make your own API logic
 
         $params = [];
         $body = \json_encode($params);
 
-        $jsonResult = \json_decode($this->apiClient->request('GET', '/api/users', ['body' => $body])->getBody(),true);
+        $jsonResult = \json_decode($this->apiClient->request('GET', '/api/users', ['body' => $body])->getBody(), true);
 
         $result = new StandardResult();
         $result->setNbTotalRows($jsonResult['total']);
         $result->setNbFilteredRows($jsonResult['total']);
 
         // build your response mapping
-        foreach($jsonResult['data'] as $row) {
+        foreach ($jsonResult['data'] as $row) {
             // you can map data here, or create cool objects
             $result->addRow($row);
         }
-
-        dump($result->getRows());
-
 
         // and return the result
         return $result;
